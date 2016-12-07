e9d649418201a2bd11ed92ba8946e99a70efce95
Grigory Ulanov <grigory.ulanov@touchin.ru>
2016-12-07 20:30:39 +0300

    fixes

--- TUCalendar/TUCalendar/Views/TUCalendarDayView.m
+++ TUCalendar/TUCalendar/Views/TUCalendarDayView.m
@@ -188,8 +188,8 @@ static CGFloat const kTodayLabelHeight = 12.f;
 
     [self updateBackgroundState];
 
-    BOOL isLeftBackgroundViewShown = (state.selectionOptions & TUCalendarDayViewSelectionLeftFull) && !state.isInvisibleDay;
-    BOOL isRightBackgroundViewShown = (state.selectionOptions & TUCalendarDayViewSelectionRightFull) && !state.isInvisibleDay;
+    BOOL isLeftBackgroundViewShown = (state.selectionOptions & TUCalendarDayViewSelectionLeftFull);
+    BOOL isRightBackgroundViewShown = (state.selectionOptions & TUCalendarDayViewSelectionRightFull);
     BOOL isRangingText = isLeftBackgroundViewShown || isRightBackgroundViewShown || [self isTextRangeState];
 
     self.leftBackgroundView.hidden = !isLeftBackgroundViewShown;
